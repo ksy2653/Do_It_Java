e5476a4610cfd8f8c1358237d59f5111219dc3dd
ksy2653 <kim2653seok@gmail.com>
2024-01-16 01:59:36 +0900

    eclipse to intellij

---
+++ src/main/java/ksy2653/Main.java
@@ -0,0 +1,33 @@
+package ksy2653;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int dNum = Integer.parseInt(stringTokenizer.nextToken());
+        int qNum = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] S = new long[dNum+1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i =1 ;i<=dNum; i++) {
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for(int q =0; q<qNum; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+
+            System.out.println(S[j] - S[i-1]);
+        }
+    }
+
+}
