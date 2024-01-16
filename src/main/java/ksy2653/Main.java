da5fb3e650f686658ae809ef660610527353c57f
ksy2653 <kim2653seok@gmail.com>
2024-01-17 01:58:27 +0900

    feat: add Q10986, Q2018

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -1,42 +1,33 @@
 package ksy2653;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] S = new int[N];
+        int[] C = new int[M];
+        S[0] = sc.nextInt();
 
-        int[][] A = new int[N+1][N+1];
-        for(int i=1;i<=N;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=1;j<=N;j++){
-                A[i][j] = Integer.parseInt(st.nextToken());
-            }
+        for(int i=1;i<N;i++){
+            S[i] = S[i-1] + sc.nextInt();
         }
-        int[][] D = new int[N+1][N+1];
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=N;j++){
-                D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j];
-            }
+        int answer=0;
+        for(int i=0;i<N;i++){
+            int remainder = (int) (S[i] % M);
+            if(remainder ==0)
+                answer++;
+            C[remainder]++;
         }
 
         for(int i=0;i<M;i++){
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            int result = D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1];
-            System.out.println(result);
+            if(C[i]>1)
+                answer = answer + (C[i]*(C[i]-1))/2;
         }
+        System.out.print(answer);
     }
 
 }
