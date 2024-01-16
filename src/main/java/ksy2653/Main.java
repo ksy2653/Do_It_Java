147fb99286a3cfc213a73c6ced9ead08e50db5c1
ksy2653 <kim2653seok@gmail.com>
2024-01-17 02:34:59 +0900

    feat: add Q1940

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -7,27 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] S = new int[N];
-        int[] C = new int[M];
-        S[0] = sc.nextInt();
+        int count =1;
+        int sum=1;
+        int start_index=1;
+        int end_index=1;
 
-        for(int i=1;i<N;i++){
-            S[i] = S[i-1] + sc.nextInt();
-        }
-        int answer=0;
-        for(int i=0;i<N;i++){
-            int remainder = (int) (S[i] % M);
-            if(remainder ==0)
-                answer++;
-            C[remainder]++;
+        while(end_index !=N){
+            if (sum == N){
+                count++;
+                end_index++;
+                sum = sum + end_index;
+            }
+            else if (sum > N){
+                sum = sum - start_index;
+                start_index++;
+            }
+            else {
+                end_index++;
+                sum = sum + end_index;
+            }
         }
 
-        for(int i=0;i<M;i++){
-            if(C[i]>1)
-                answer = answer + (C[i]*(C[i]-1))/2;
-        }
-        System.out.print(answer);
+        System.out.print(count);
     }
 
 }
