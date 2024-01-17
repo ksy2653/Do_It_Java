7690222e8a7e9f82deeddd43f90ab9279be9734c
ksy2653 <kim2653seok@gmail.com>
2024-01-17 21:43:35 +0900

    feat: add Q1253

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -1,34 +1,43 @@
 package ksy2653;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int count =1;
-        int sum=1;
-        int start_index=1;
-        int end_index=1;
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int S[] = new int[N];
+        for(int i=0;i<N;i++){
+            S[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(S);
 
-        while(end_index !=N){
-            if (sum == N){
+        int count=0;
+        int start_index=0;
+        int end_index=N-1;
+        while (end_index > start_index){
+            int sum = S[start_index] + S[end_index];
+            if (sum == M){
                 count++;
-                end_index++;
-                sum = sum + end_index;
-            }
-            else if (sum > N){
-                sum = sum - start_index;
                 start_index++;
+                end_index--;
             }
-            else {
-                end_index++;
-                sum = sum + end_index;
-            }
+            else if (sum > M)
+                end_index--;
+            else
+                start_index++;
         }
 
         System.out.print(count);
+        bf.close();
     }
-
 }
