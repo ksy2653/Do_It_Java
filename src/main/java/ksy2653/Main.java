84bd36c0769ba03a19a1188677ea6b0a05cf563e
ksy2653 <kim2653seok@gmail.com>
2024-02-07 22:30:47 +0900

    feat: Q17298

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -1,43 +1,47 @@
 package ksy2653;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.io.IOException;
 import java.util.Scanner;
-
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int M = Integer.parseInt(bf.readLine());
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int S[] = new int[N];
-        for(int i=0;i<N;i++){
-            S[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i =0;i<A.length;i++){
+            A[i] = sc.nextInt();
         }
-        Arrays.sort(S);
 
-        int count=0;
-        int start_index=0;
-        int end_index=N-1;
-        while (end_index > start_index){
-            int sum = S[start_index] + S[end_index];
-            if (sum == M){
-                count++;
-                start_index++;
-                end_index--;
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int num=1;
+        boolean result = true;
+        for(int i=0;i<A.length;i++){
+            int su = A[i];
+            if(su>=num){
+                while(su >= num){
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
             }
-            else if (sum > M)
-                end_index--;
-            else
-                start_index++;
+            else{
+                if(su != stack.peek()){
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }
+                else{
+                    stack.pop();
+                    bf.append("-\n");
+                }
+            }
         }
 
-        System.out.print(count);
-        bf.close();
+        if(result)
+            System.out.println(bf.toString());
+
     }
+
 }
