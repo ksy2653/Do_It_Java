946d241a4369d7057bc88e264398cddb7edb9b4e
ksy2653 <kim2653seok@gmail.com>
2024-02-10 17:03:06 +0900

    feat: add Q2750

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -1,24 +1,39 @@
 package ksy2653;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Queue<Integer> myQueue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i=1;i<=N;i++){
-            myQueue.add(i);
-        }
-        while(myQueue.size()>1){
-            myQueue.poll();
-            myQueue.add(myQueue.poll());
-        }
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>(((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
 
-        System.out.println(myQueue.poll());
+            if(first_abs == second_abs){
+                return o1 > o2 ? 1 : -1;
+            }
+            else
+                return first_abs - second_abs;
+        }));
+
+        for(int i=0;i<N;i++){
+            int request = Integer.parseInt(br.readLine());
+            if(request == 0){
+                if(!myQueue.isEmpty()){
+                    System.out.println(myQueue.poll());
+                }
+                else
+                    System.out.println("0");
+            }
+            else
+                myQueue.add(request);
+        }
     }
 
 }
