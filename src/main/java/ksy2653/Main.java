503c3d389615eb94131147351364223154ce8fe5
ksy2653 <kim2653seok@gmail.com>
2024-02-11 15:24:42 +0900

    feat: add Q1427

--- src/main/java/ksy2653/Main.java
+++ src/main/java/ksy2653/Main.java
@@ -1,29 +1,42 @@
 package ksy2653;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        for(int i=0;i<N;i++){
-            A[i] = sc.nextInt();
-        }
-
-        for(int i=0;i<N-1;i++){
-            for(int j=0;j<N-1-i;j++){
-                if(A[j] > A[j+1]){
-                    int temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
-                }
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        mData[] A = new mData[N];
 
         for(int i=0;i<N;i++){
-            System.out.println(A[i]);
+            A[i] = new mData((Integer.parseInt(br.readLine())),i);
         }
+
+        Arrays.sort(A);
+        int Max=0;
+        for(int i=0;i<N;i++){
+            if(A[i].index - i > Max)
+                Max = A[i].index-i;
+        }
+
+        System.out.println(Max+1);
+    }
+}
+
+class mData implements Comparable<mData> {
+    int value;
+    int index;
+    public mData(int value, int index){
+        super();
+        this.value = value;
+        this.index = index;
     }
 
+    @Override
+    public int compareTo(mData o){
+        return this.value - o.value;
+    }
 }
