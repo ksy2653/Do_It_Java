887e0bfd14d1d82c3866ff9b5648d7f969412b21
ksy2653 <kim2653seok@gmail.com>
2024-07-06 18:10:05 +0900

    feat: Q2023

--- doit_coding_test/src/ch05/primeNum/PrimeNum.java
+++ doit_coding_test/src/ch05/primeNum/PrimeNum.java
@@ -1,10 +1,51 @@
 package ch05.primeNum;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PrimeNum {
+	static int N;
+	static ArrayList<Integer> primeList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		System.setIn(new FileInputStream("src/ch05/primeNum/input.txt"));
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		primeList = new ArrayList<>();
+		
+		dfs(2,1);
+		dfs(3,1);
+		dfs(5,1);
+		dfs(7,1);
+		
+		for(int i=0;i<primeList.size();i++) {
+			System.out.println(primeList.get(i));
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		if(num<=1) 
+			return false;
+		if(num==2)
+			return true;
+		for(int i=2;i<num;i++) {
+			if(num%i == 0)
+				return false;
+		}
+		
+		return true;
 	}
 
+	public static void dfs(int i, int jarisu) {
+		if(jarisu == N)
+			primeList.add(i);
+		for(int j=1;j<=9;j+=2) {
+			int temp = 10*i+j;
+			if(isPrime(temp))
+				dfs(temp,jarisu+1);
+		}
+	}
 }
--- doit_coding_test/src/main/Main.java
+++ doit_coding_test/src/main/Main.java
@@ -4,9 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N,M;
+	static int count=0;
+	static boolean visited[];
+	static ArrayList<Integer> connected[];
 
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/main/input.txt"));
@@ -14,8 +19,43 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println(1);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		connected = new ArrayList[N+1];
+		visited = new boolean[N+1];
 		
+		for(int i=1;i<=N;i++) {
+			connected[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i=1;i<=M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			
+			connected[s].add(e);
+			connected[e].add(s);
+		}
+		
+		for(int i=1;i<=N;i++) {
+			if(!visited[i]) {
+				count++;
+				dfs(i);
+			}
+		}
+		
+		System.out.println(count);
 	}
 	
+	public static void dfs(int v) {
+		if(visited[v])
+			return;
+		
+		visited[v] = true;
+		for(int i:connected[v]) {
+			if(!visited[i]) {
+				dfs(i);
+			}
+		}
+	}
 }
