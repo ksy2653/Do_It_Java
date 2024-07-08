c9ca3d5f4f0b44ac090925ee5baebcc86eb48bd3
ksy2653 <kim2653seok@gmail.com>
2024-07-08 16:38:41 +0900

    feat: Q유기농배추

--- doit_coding_test/src/main/Main.java
+++ doit_coding_test/src/main/Main.java
@@ -8,10 +8,11 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M;
-	static int count=0;
-	static boolean visited[];
-	static ArrayList<Integer> connected[];
+	static int T,N,M,K;
+	static boolean[][] visited, cabbage;
+	static int worm;
+	static int dx[] = {0,0,-1,1};
+	static int dy[] = {-1,1,0,0};
 
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/main/input.txt"));
@@ -19,43 +20,47 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		connected = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		
-		for(int i=1;i<=N;i++) {
-			connected[i] = new ArrayList<Integer>();
-		}
-		
-		for(int i=1;i<=M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+		//worm_list = new ArrayList<>();
+		T = Integer.parseInt(st.nextToken());
+		for(int i=0;i<T;i++) {
+			worm =0;
 			
-			connected[s].add(e);
-			connected[e].add(s);
-		}
-		
-		for(int i=1;i<=N;i++) {
-			if(!visited[i]) {
-				count++;
-				dfs(i);
+			st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			cabbage = new boolean[N][M];
+			visited = new boolean[N][M];
+			
+			for(int j=0;j<K;j++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				cabbage[y][x] = true;
 			}
+			
+			for(int j=0;j<N;j++) {
+				for(int k=0;k<M;k++) {
+					if(!visited[j][k] && cabbage[j][k]) {
+						worm++;
+						dfs(j,k);
+					}
+				}
+			}
+			System.out.println(worm);
 		}
-		
-		System.out.println(count);
 	}
 	
-	public static void dfs(int v) {
-		if(visited[v])
+	public static void dfs(int x, int y) {
+		if(visited[x][y] || !cabbage[x][y])
 			return;
-		
-		visited[v] = true;
-		for(int i:connected[v]) {
-			if(!visited[i]) {
-				dfs(i);
-			}
+		visited[x][y] = true;
+		for(int i=0;i<4;i++) {
+			int newX = dx[i] + x;
+			int newY = dy[i] + y;
+			if(0<= newX && newX <N && 0<=newY && newY<M && !visited[newX][newY] && cabbage[newX][newY])
+				dfs(newX,newY);
 		}
 	}
 }
