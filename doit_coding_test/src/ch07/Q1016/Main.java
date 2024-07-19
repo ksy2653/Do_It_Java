e7fe7ca8e3a3fd84bed6791184c346390007ffdd
ksy2653 <kim2653seok@gmail.com>
2024-07-20 00:55:51 +0900

    feat: add Q11689

--- doit_coding_test/src/ch07/Q1016/Main.java
+++ doit_coding_test/src/ch07/Q1016/Main.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 	
 		long min = sc.nextLong();
 		long max = sc.nextLong();
 		
 		boolean[] check = new boolean[(int)(max-min+1)];
-		for(int i=2;i*i<=max;i++) {
-			int pow = i*i;
-			int start_index = (int)min/pow;
+		for(long i=2;i*i<=max;i++) {
+			long pow = i*i;
+			long start_index = min/pow;
 			if(min%pow !=0)
 				start_index++;
 			
-			for(int j=start_index;j*pow<=max;j++) {
-				check[(int)(j*pow-min)]=true;
+			for(long j=start_index;j*pow<=max;j++) {
+				check[(int)((j*pow)-min)]=true;
 			}
 		}
 		
