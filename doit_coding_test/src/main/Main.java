689ef801f439f9b4a3f5ea8d2ab494bc17bcc60e
ksy2653 <kim2653seok@gmail.com>
2024-07-22 23:14:23 +0900

    feat: add Q1978

--- doit_coding_test/src/main/Main.java
+++ doit_coding_test/src/main/Main.java
@@ -11,32 +11,35 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		long N = sc.nextLong();
-		long A[] = new long[(int)N];
+		int N = sc.nextInt();
+		int count=0;
 		for (int i = 0; i < N; i++) {
-			A[i] = sc.nextLong();
-		}
-		
-		long B = sc.nextLong();
-		long C = sc.nextLong();
-		
-		long count=0;
-		for (int i = 0; i < N; i++) {
-			if(A[i]<=B)
+			int num=sc.nextInt();
+			if(isPrime(num))
 				count++;
-			else {
-				count++;
-				A[i] -=B;
-				if(A[i] % C == 0) {
-					count = count + (A[i] / C);
-				}
-				else
-					count = count + (A[i] / C) + 1;
-			}
 		}
 		
 		System.out.println(count);
 		
 	}
 	
+	public static boolean isPrime(int num) {
+		int[] A = new int[num+1];
+		for(int i=2;i<=num;i++) {
+			A[i]=i;
+		}
+		
+		for(int i=2;i<=Math.sqrt(num);i++) {
+			if(A[i]==0)
+				continue;
+			for(int j=i*2;j<=num;j=j+i) {
+				A[j]=0;
+			}
+		}
+		
+		if(A[num]==0)
+			return false;
+		else
+			return true;
+	}
 }
