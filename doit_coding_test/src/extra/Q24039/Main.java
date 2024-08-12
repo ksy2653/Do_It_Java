a7f00e853eac6ae1404dabfc40febe80918b22d5
ksy2653 <kim2653seok@gmail.com>
2024-08-12 22:41:25 +0900

    feat: add Q24039

---
+++ doit_coding_test/src/extra/Q24039/Main.java
@@ -0,0 +1,39 @@
+package extra.Q24039;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//int N = sc.nextInt();
+		
+//		int[] prime = new int[N+1];
+//		for(int i=1;i<=N;i++) {
+//			prime[i]=i;
+//		}
+//		for(int i=2;i<=Math.sqrt(N);i++) {
+//			if(prime[i]==0)
+//				continue;
+//			for(int j=i*2;j<=N;j=j+i) {
+//				prime[j]=0;
+//			}
+//		}
+//
+//		
+//		int result=1;
+//		int count=0;
+//
+//		for(int i=(int)Math.sqrt(N)-1;i<=N;i++) {
+//			if(prime[i]!=0) {
+//				result = result * prime[i];
+//				count++;
+//			}
+//			if(count==2)
+//				break;
+		}
+		//System.out.println(result);
+		System.out.println(X);
+	}
+}
