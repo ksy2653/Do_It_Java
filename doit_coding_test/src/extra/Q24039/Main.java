359716c5a442195dc4302a1785ceb2588a3cade6
ksy2653 <kim2653seok@gmail.com>
2024-08-14 16:15:27 +0900

    fix: Q24039 solution

--- doit_coding_test/src/extra/Q24039/Main.java
+++ doit_coding_test/src/extra/Q24039/Main.java
@@ -1,6 +1,5 @@
 package extra.Q24039;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,36 +8,35 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		
-		int[] prime = new int[N+1];
-		for(int i=1;i<=N;i++) {
+		int[] prime = new int[10001];
+		for(int i=1;i<=10000;i++) {
 			prime[i]=i;
 		}
-		for(int i=2;i<=Math.sqrt(N);i++) {
+		for(int i=2;i<=Math.sqrt(10000);i++) {
 			if(prime[i]==0)
 				continue;
-			for(int j=i*2;j<=N;j=j+i) {
+			for(int j=i*2;j<=10000;j=j+i) {
 				prime[j]=0;
 			}
 		}
 
-		
-		int result=0;
-		int count=1;
-
-		for(int i=2;i<=N;i++) {
-			if(prime[i]*prime[i] > N) {
-				result=i;
+		for(int i = 2;i<=10000;i++) {
+			if(prime[i]==0)
+				continue;
+			int now = i;
+			boolean isPrime=false;
+			while(isPrime==false) {
+				now++;
+				if(prime[now] != 0) {
+					isPrime=true;
+				}
+			}
+			
+			if(prime[i] * prime[now] > N) {
+				System.out.println(prime[i]*prime[now]);
 				break;
 			}
+			
 		}
-		
-		for(int i=result-1;i>2;i--) {
-			if(prime[i]!=0) {
-				count = result*i;
-				break;
-			}
-		}
-		
-		System.out.println(count);
 	}
 }
