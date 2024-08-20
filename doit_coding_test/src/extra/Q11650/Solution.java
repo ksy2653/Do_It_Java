8b38fd175d8dbafa283e0d81fec275dad122e085
ksy2653 <kim2653seok@gmail.com>
2024-08-20 16:38:43 +0900

    feat: add Q2839

--- doit_coding_test/src/extra/Q11650/Solution.java
+++ doit_coding_test/src/extra/Q11650/Solution.java
@@ -1,9 +1,33 @@
 package extra.Q11650;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.nextInt();
+			arr[i][1] = in.nextInt();
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
 
 	}
 
