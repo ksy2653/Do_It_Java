d7edb3846ee059240e62737e040e69a4d71fd7e0
ksy2653 <kim2653seok@gmail.com>
2024-08-23 22:37:09 +0900

    feat: add Q1446

---
+++ doit_coding_test/src/extra/Q1446/Main.java
@@ -0,0 +1,75 @@
+package extra.Q1446;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,D;
+	static int[] distance;
+	static ArrayList<Node>[] list;
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/extra/Q1446/input.txt"));
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		
+		distance = new int[10001];
+		for(int i=0;i<=10000;i++) {
+			distance[i]=i;
+		}
+		
+		list = new ArrayList[D+1];
+		for(int i=0;i<=D;i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			list[s].add(new Node(e,v));
+		}
+		
+		dijkstra(0);
+		System.out.println(distance[D]);
+	}
+
+	public static void dijkstra(int start) {
+		if(start>D)
+			return;
+		if(distance[start+1] > distance[start] + 1) 
+			distance[start+1] = distance[start]+1;
+		
+		for(int i=0;i<list[start].size();i++) {
+			Node temp = list[start].get(i);
+			int next = temp.node;
+			int dist = temp.weight;
+			
+			if(distance[next] > distance[start] + dist) {
+				distance[next] = distance[start] + dist;
+			}
+		}
+		
+		dijkstra(start+1);
+	}
+}
+
+class Node{
+	int node;
+	int weight;
+	
+	public Node(int node, int weight) {
+		this.node = node;
+		this.weight = weight;
+	}
+}
--- doit_coding_test/src/main/Main.java
+++ doit_coding_test/src/main/Main.java
@@ -1,75 +1,88 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int V,E,K;
+	static int N,M;
 	static int[] distance;
 	static boolean[] visited;
 	static ArrayList<Edge>[] list;
-	static PriorityQueue<Edge> q;
-	
+
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/main/input.txt"));
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(br.readLine());
 		
-		distance = new int[V+1];
-		visited = new boolean[V+1];
-		list = new ArrayList[V+1];
-	
-		for(int i=1;i<=V;i++) {
-			distance[i] = Integer.MAX_VALUE;
+		distance = new int[N+1];
+		visited = new boolean[N+1];
+		list = new ArrayList[N+1];
+		
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		for(int i=1;i<=N;i++) {
 			list[i] = new ArrayList<Edge>();
 		}
 		
-		for(int i=0;i<E;i++) {
+		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
-			int u = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			
-			list[s].add(new Edge(u,v));
+			list[s].add(new Edge(e,v));
 		}
 		
-		distance[K]=0;
-		q = new PriorityQueue<Edge>();
-		q.add(new Edge(K,0));
+		st = new StringTokenizer(br.readLine());
+		int startIndex = Integer.parseInt(st.nextToken());
+		int endIndex = Integer.parseInt(st.nextToken());
 		
-		while(!q.isEmpty()) {
-			Edge current = q.poll();
-			int cur_vertex = current.vertex;
-			if(visited[cur_vertex])
-				continue;
-			visited[cur_vertex] = true;
+		int result = dijkstra(startIndex,endIndex);
+		bw.write(result+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	public static int dijkstra(int start, int end) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start,0));
+		distance[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Edge current = pq.poll();
+			int c_v = current.vertex;
 			
-			for(Edge temp: list[cur_vertex]) {
+			if(visited[c_v])
+				continue;
+			visited[c_v] = true;
+			
+			for(Edge temp:list[c_v]) {
 				int next = temp.vertex;
 				int value = temp.value;
 				
-				distance[next] = Math.min(distance[next], distance[cur_vertex] + value);
-				q.add(new Edge(next,distance[next]));
+				if(distance[next] > distance[c_v] + value) {
+					distance[next] = distance[c_v] + value;
+					pq.add(new Edge(next,distance[next]));
+				}
 			}
 		}
 		
-		for(int i=1;i<=V;i++) {
-			if(visited[i])
-				System.out.println(distance[i]);
-			else
-				System.out.println("INF");
-		}
+		return distance[end];
 	}
+		
 }
 
 class Edge implements Comparable<Edge>{
@@ -82,6 +95,6 @@ class Edge implements Comparable<Edge>{
 	}
 	
 	public int compareTo(Edge e) {
-		return this.value - e.value;
+		return value - e.value;
 	}
 }
