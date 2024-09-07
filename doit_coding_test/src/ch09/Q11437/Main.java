4c8a9b37c605abf38749e79b49a72b953ea46421
ksy2653 <kim2653seok@gmail.com>
2024-09-07 18:19:07 +0900

    feat: add Q11437

---
+++ doit_coding_test/src/ch09/Q11437/Main.java
@@ -0,0 +1,98 @@
+package ch09.Q11437;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static ArrayList<Integer>[] tree;
+	static int[] depth;
+	static int[] parent;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("src/ch09/Q11437/input.txt"));
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		tree = new ArrayList[N+1];
+		for(int i=1;i<=N;i++) {
+			tree[i] = new ArrayList<>();
+		}
+		
+		for(int i=0;i<N-1;i++) {
+			int s = sc.nextInt();
+			int e = sc.nextInt();
+			tree[s].add(e);
+			tree[e].add(s);
+		}
+		
+		depth = new int[N+1];
+		parent = new int[N+1];
+		visited = new boolean[N+1];
+		
+		bfs(1);
+		
+		int M = sc.nextInt();
+		for(int i=0;i<M;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int LCA = excuteLCA(a,b);
+			
+			System.out.println(LCA);
+		}
+		
+	}
+	
+	public static int excuteLCA(int a, int b) {
+		if(depth[a]<depth[b]) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		
+		while(depth[a] != depth[b]) {
+			a = parent[a];
+		}
+		
+		while(a!=b) {
+			a = parent[a];
+			b = parent[b];
+		}
+		
+		return a;
+	}
+
+	public static void bfs(int node) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(node);
+		visited[node] = true;
+		
+		int level = 1;
+		int now_size=1;
+		int count=0;
+		
+		while(!queue.isEmpty()) {
+			int now_node = queue.poll();
+			for(int next:tree[now_node]) {
+				if(!visited[next]) {
+					visited[next]=true;
+					queue.add(next);
+					parent[next] = now_node;
+					depth[next]=level;
+				}
+			}
+			
+			count++;
+			if(count==now_size) {
+				count=0;
+				now_size=queue.size();
+				level++;
+			}
+		}
+	}
+}
