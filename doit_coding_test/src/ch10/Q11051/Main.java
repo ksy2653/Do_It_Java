9bb1a25d3106968e606bf456a3067239dd6d4263
ksy2653 <kim2653seok@gmail.com>
2024-09-08 14:29:59 +0900

    feat: add Q11051

---
+++ doit_coding_test/src/ch10/Q11051/Main.java
@@ -0,0 +1,32 @@
+package ch10.Q11051;
+
+import java.util.Scanner;
+
+public class Main {
+	static int N,K;
+	static int[][] dp;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		K = sc.nextInt();
+		dp = new int[N+1][N+1];
+		
+		for(int i=0;i<=N;i++) {
+			dp[i][i]=1;
+			dp[i][1]=i;
+			dp[i][0]=1;
+		}
+		
+		for(int i=2;i<=N;i++) {
+			for(int j=2;j<i;j++) {
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+				dp[i][j] = dp[i][j] % 10007;
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+	}
+
+}
