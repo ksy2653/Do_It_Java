48ce0cce348cdb9da8c6ec49846003885a9c01e0
ksy2653 <kim2653seok@gmail.com>
2024-10-02 01:37:36 +0900

    feat: add 무인도 여행 programmers

---
+++ doit_coding_test/src/programmers/무인도_여행/Solution.java
@@ -0,0 +1,19 @@
+package programmers.무인도_여행;
+
+public class Solution {
+	public int[] solution(String[] maps) {
+		int[] answer = {};
+		
+		String[] temp = {"X591X","X1X5X","X231X", "1XXX1"};
+		int a = temp.length;
+		int b = temp[0].length();
+		return answer;
+	}
+	public static void main(String[] args) {
+		String[] temp = {"X591X","X1X5X","X231X", "1XXX1"};
+		
+		System.out.println(temp[0]);
+		System.out.println(temp[0].charAt(0)=='X');
+	}
+
+}
