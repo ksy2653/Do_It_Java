8fa1027265737496ddf03881d980b87362631734
ksy2653 <kim2653seok@gmail.com>
2024-10-02 21:40:16 +0900

    feat: add 직각다각형 groom

--- doit_coding_test/src/programmers/무인도_여행/Solution.java
+++ doit_coding_test/src/programmers/무인도_여행/Solution.java
@@ -1,19 +1,72 @@
 package programmers.무인도_여행;
 
-public class Solution {
-	public int[] solution(String[] maps) {
-		int[] answer = {};
-		
-		String[] temp = {"X591X","X1X5X","X231X", "1XXX1"};
-		int a = temp.length;
-		int b = temp[0].length();
-		return answer;
-	}
-	public static void main(String[] args) {
-		String[] temp = {"X591X","X1X5X","X231X", "1XXX1"};
-		
-		System.out.println(temp[0]);
-		System.out.println(temp[0].charAt(0)=='X');
-	}
+import java.util.*;
 
+class Solution {
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+    static boolean[][] visited;
+    static int[][] map;
+    static int row,col,food;
+    
+    public int[] solution(String[] maps) {
+        row = maps.length;
+        col = maps[0].length();
+        
+        visited = new boolean[row][col];
+        map = new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(maps[i].charAt(j) == 'X'){
+                    map[i][j] = 0;
+                }
+                else
+                    map[i][j] = maps[i].charAt(j) - '0';
+            }
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+                
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(!visited[i][j] && map[i][j] != 0){
+                    food=map[i][j];
+                    dfs(i,j);
+                    list.add(food);
+                }
+                
+            }
+        }
+        if(list.size()==0){
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);       
+        }
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+    
+    public static void dfs(int x, int y){
+        if(visited[x][y])
+            return;
+        
+        visited[x][y] = true;
+        
+        for(int i=0;i<4;i++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            
+            if(0<=newX && newX<row && 0<=newY && newY <col 
+               && map[newX][newY] != 0 && !visited[newX][newY]) {
+                
+                food += map[newX][newY];
+                dfs(newX,newY);
+            }
+        }
+    }
 }
