5e70dd481b56a11404f04ad352bf72c78306e6fe
ksy2653 <kim2653seok@gmail.com>
2024-10-05 16:27:59 +0900

    feat: add 피보나치 수 groom

--- doit_coding_test/src/groom/구름이의_취미/Main.java
+++ doit_coding_test/src/groom/구름이의_취미/Main.java
@@ -1,5 +1,18 @@
 package groom.구름이의_취미;
 
-public class Main {
+import java.util.Scanner;
 
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		long mod = 1000000007;
+		
+		// 공식: (N * (N + 1) / 2)^2 % mod
+		long sum = N * (N + 1) / 2;   // N(N+1)/2의 값을 계산
+		sum %= mod;                   // mod 연산을 먼저 적용
+		long V_sum = (sum * sum) % mod;  // 최종적으로 세제곱의 합을 구함
+		
+		System.out.println(V_sum);
+	}
 }
